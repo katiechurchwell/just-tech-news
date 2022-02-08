190ccd68352a8ff7831344fb7ef3f08010f27b1c
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-07 19:45:26 -0800

    connect to jawsdb

---
+++ README.md
@@ -0,0 +1,9 @@
+# Just Tech News
+A recreation of the [Hacker News]("https://news.ycombinator.com/") website to practice API building. 
+
+The user can create an account, login, post, comment and upvote posts.
+
+Uses [Sequelize](https://sequelize.org/master/index.html) and [Express](https://expressjs.com/).
+
+## Deployment
+https://nameless-refuge-06666.herokuapp.com/
--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+let sequelize;
 
-sequelize.authenticate().then(()=> {
-  console.log("Connection has been stablised successfully.");
-  }).catch(err => {
-  console.log("Unable to connect to the database" , err);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been stablised successfully.");
   })
+  .catch((err) => {
+    console.log("Unable to connect to the database", err);
+  });
 
 module.exports = sequelize;
