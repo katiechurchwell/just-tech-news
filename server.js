2f70abfcebc447e9906715a24aa0a6c07e2a8106
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-14 01:13:42 -0800

    heroku_secret

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'canyouguessmysecret',
+  secret: Heroku_secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
